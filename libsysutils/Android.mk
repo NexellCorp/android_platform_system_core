09e5055a5c235ddfcb83c860e3fb5510313d15f6
allan.park <allan.park@nexell.co.kr>
2016-02-17 11:04:14 +0900

    [ADD] fix the mincrypt to apply exFAT

--- libsysutils/Android.mk
+++ libsysutils/Android.mk
@@ -24,4 +24,21 @@ LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 include $(BUILD_SHARED_LIBRARY)
 
+# psw0523 add for vold ntfs-exfat-cm_11.0-version
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:=                             \
+                  src/SocketListener.cpp      \
+                  src/FrameworkListener.cpp   \
+                  src/NetlinkListener.cpp     \
+                  src/NetlinkEvent.cpp        \
+                  src/FrameworkCommand.cpp    \
+                  src/SocketClient.cpp        \
+                  src/ServiceManager.cpp      \
+                  EventLogTags.logtags
+LOCAL_MODULE:= libsysutils
+LOCAL_C_INCLUDES := $(KERNEL_HEADERS)
+LOCAL_CFLAGS :=
+include $(BUILD_STATIC_LIBRARY)
+# end psw0523
+
 endif
