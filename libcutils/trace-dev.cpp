ae351c8ac8d84cbfddc7f7c9813c8397f7bb211f
Sungwoo Park <swpark@nexell.co.kr>
2019-03-07 16:52:05 +0900

    nexell/quickboot/libcutils: Ignore all atrace api
    This patch removes all implementation code for atrace.
    
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>
    Change-Id: I781413721d48bb334baa070412ae6bd802fb0c16

--- libcutils/trace-dev.cpp
+++ libcutils/trace-dev.cpp
@@ -18,62 +18,36 @@
 
 #include "trace-dev.inc"
 
-static pthread_once_t atrace_once_control = PTHREAD_ONCE_INIT;
-
 // Set whether tracing is enabled in this process.  This is used to prevent
 // the Zygote process from tracing.
-void atrace_set_tracing_enabled(bool enabled)
+void atrace_set_tracing_enabled(bool)
 {
-    atomic_store_explicit(&atrace_is_enabled, enabled, memory_order_release);
-    atrace_update_tags();
-}
-
-static void atrace_init_once()
-{
-    atrace_marker_fd = open("/sys/kernel/debug/tracing/trace_marker", O_WRONLY | O_CLOEXEC);
-    if (atrace_marker_fd == -1) {
-        ALOGE("Error opening trace file: %s (%d)", strerror(errno), errno);
-        atrace_enabled_tags = 0;
-        goto done;
-    }
-
-    atrace_enabled_tags = atrace_get_property();
-
-done:
-    atomic_store_explicit(&atrace_is_ready, true, memory_order_release);
 }
 
 void atrace_setup()
 {
-    pthread_once(&atrace_once_control, atrace_init_once);
 }
 
-void atrace_begin_body(const char* name)
+void atrace_begin_body(const char*)
 {
-    WRITE_MSG("B|%d|", "%s", name, "");
 }
 
 void atrace_end_body()
 {
-    WRITE_MSG("E|%d", "%s", "", "");
 }
 
-void atrace_async_begin_body(const char* name, int32_t cookie)
+void atrace_async_begin_body(const char*, int32_t)
 {
-    WRITE_MSG("S|%d|", "|%" PRId32, name, cookie);
 }
 
-void atrace_async_end_body(const char* name, int32_t cookie)
+void atrace_async_end_body(const char*, int32_t)
 {
-    WRITE_MSG("F|%d|", "|%" PRId32, name, cookie);
 }
 
-void atrace_int_body(const char* name, int32_t value)
+void atrace_int_body(const char*, int32_t)
 {
-    WRITE_MSG("C|%d|", "|%" PRId32, name, value);
 }
 
-void atrace_int64_body(const char* name, int64_t value)
+void atrace_int64_body(const char*, int64_t)
 {
-    WRITE_MSG("C|%d|", "|%" PRId64, name, value);
 }
